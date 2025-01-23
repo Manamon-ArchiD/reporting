4d863442758fbc36be7c46abf13a8097a37a1c23
Hugo Brajdic <hugobrajdic@gmail.com>
2025-01-23 16:46:53 +0100

    Fix port 3001

--- src/routes/reporting.routes.ts
+++ src/routes/reporting.routes.ts
@@ -19,4 +19,21 @@ router.get('/detailed', (req, res) => {
   });
 });
 
+/**
+ * @swagger
+ * /report/export:
+ *   get:
+ *     summary: Générer et renvoyer un rapport détaillé au format csv
+ *     description: Ce point d'API génère un rapport complet avec les statistiques globales et retourne un fichier .csv
+ *     responses:
+ *       200:
+ *         description: Rapport csv généré avec succès.
+ */
+router.get('/export', (req, res) => {
+  res.json({
+    reportId: '123',
+    reportData: [],
+  });
+});
+
 export default router;
