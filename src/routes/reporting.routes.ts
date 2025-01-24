3ebcaf7c98ee255a7f1c3ffb03b6729db50e2245
Hugo Brajdic <hugobrajdic@gmail.com>
2025-01-24 09:50:19 +0100

    Ajout fetch sur micro service Statistiques (3000)

--- src/routes/reporting.routes.ts
+++ src/routes/reporting.routes.ts
@@ -12,12 +12,34 @@ const router = Router();
  *       200:
  *         description: Rapport généré avec succès.
  */
-router.get('/detailed', (req, res) => {
-  res.json({
-    reportId: '123',
-    reportData: [],
+router.get('/detailed', async (req, res) => {
+  try {
+      // Récupération des données d'un autre endpoint (exemple : http://localhost:5000/api/stats)
+      const response = await fetch('http://localhost:3000/stats/summarize');
+
+      if (!response.ok) {
+        throw new Error(`Erreur HTTP ${response.status}`);
+      }
+
+      const externalData = await response.json();
+
+      // Combiner ou utiliser les données récupérées pour générer le rapport
+      res.json({
+        reportId: '123',
+        reportData: externalData,
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+            // Si l'erreur est une instance d'Error, nous pouvons accéder à la propriété `message`
+            console.error('Erreur lors de la récupération des données :', error.message);
+          } else {
+            // Si ce n'est pas une instance d'Error, nous affichons une erreur générique
+            console.error('Erreur inconnue lors de la récupération des données.');
+          }
+          res.status(500).json({ error: 'Impossible de récupérer les données externes' });
+    }
   });
-});
+
 
 /**
  * @swagger
